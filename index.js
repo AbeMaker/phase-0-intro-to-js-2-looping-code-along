19a6f47d35a95ea75db219679d7ec898b7392680
Abraham Anyang <anyangmaker90@gmail.com>
2022-06-11 21:51:59 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+//for (let age = 30; age < 40; age++){
+//console.log(`I\'m ${age} years old.Happy birthday to me!`);
+//}
+
+//const gifts = ["teddy bear", "drone", "doll"];
+
+
+//function wrapGifts(gifts) {
+//for (let i = 0; i < gifts.length; i++) {
+//console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//}
+
+//return gifts;
+//}
+//wrapGifts(gifts);
+function writeCards(newArray, event) {
+    let thankYouCards = [];
+    for (let i = 0; i < newArray.length; i++) {
+      thankYouCards.push(
+        `Thank you, ${newArray[i]}, for the wonderful ${event} gift!`
+      );
+    }
+    return thankYouCards;
+  }
+  function countDown(Number) {
+    while (Number > 0) {
+      console.log(Number);
+      Number -= 1;
+    }
+    console.log(Number);
+  }
